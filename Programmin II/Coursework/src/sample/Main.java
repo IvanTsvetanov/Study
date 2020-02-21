d10c658e634656a20e054d93f8f11ff6516f878e
tsvetanov i. (it4u19) <it4u19@soton.ac.uk>
2020-02-21 14:51:37 +0000

    Done first part of programming coursewok

--- Programmin II/Coursework/src/sample/Main.java
+++ Programmin II/Coursework/src/sample/Main.java
@@ -29,6 +29,8 @@ public class Main extends Application {
         //Create the canvas and add it to its pane
         Pane canvasPane = new GridPane();
         Canvas canvas = new Canvas(450, 450);
+        canvasPane.prefWidthProperty().bind(mainPane.widthProperty());
+        canvasPane.prefHeightProperty().bind(mainPane.heightProperty());
         canvasPane.getChildren().add(canvas);
 
         //Set the border of the canvas
@@ -39,23 +41,41 @@ public class Main extends Application {
                 "-fx-border-color: darkblue;");
         //endregion
 
-        //region Adding components to the Main pane
+        //region Creating components to add to the Main Frame.
         GridPane bottomComponents = new GridPane();
         bottomComponents.setPadding(new Insets(10, 10, 10, 5));
 
         //Add the bottom elements and format them
         Button undo = new Button("Undo");
         undo.setMinSize(100, 30);
+        undo.prefWidthProperty().bind(mainPane.widthProperty());
+        undo.setMaxSize(250, 60);
+
         Button redo = new Button("Redo");
         redo.setMinSize(100, 30);
+        redo.prefWidthProperty().bind(mainPane.widthProperty());
+        redo.setMaxSize(250, 60);
+
         Button clear = new Button("Clear");
         clear.setMinSize(100, 30);
+        clear.prefWidthProperty().bind(mainPane.widthProperty());
+        clear.setMaxSize(250, 60);
+
         Button showMistakes = new Button("Show Mistakes");
         showMistakes.setMinSize(100, 30);
+        showMistakes.prefWidthProperty().bind(mainPane.widthProperty());
+        showMistakes.setMaxSize(250, 60);
+
         Button solve = new Button("Solve");
         solve.setMinSize(100, 30);
+        solve.prefWidthProperty().bind(mainPane.widthProperty());
+        solve.setMaxSize(250, 60);
+
         Label timerLabel = new Label("00:00");
         timerLabel.setMinSize(100, 30);
+        timerLabel.prefWidthProperty().bind(mainPane.widthProperty());
+        timerLabel.prefHeightProperty().bind(mainPane.heightProperty());
+        timerLabel.setMaxSize(250, 60);
         timerLabel.setStyle("-fx-border-radius: 3;" +
                 "-fx-border-color: brown;");
         timerLabel.setAlignment(Pos.CENTER);
@@ -72,7 +92,38 @@ public class Main extends Application {
         bottomComponents.setHgap(80);
 
         //Add the side elements and format them
+        GridPane sideComponents = new GridPane();
 
+        sideComponents.setPadding(new Insets(10, 10, 10, 10));
+        sideComponents.setVgap(20);
+        sideComponents.setHgap(20);
+
+        int minHeight = (int)(sideComponents.heightProperty().getValue()/50);
+        Button loadFile = new Button ("Load Puzzle from File");
+        loadFile.setMinSize(150, 50);
+        loadFile.prefHeightProperty().bind(mainPane.heightProperty());
+        loadFile.setMaxSize(150, 150);
+
+        Button loadText = new Button ("Load Puzzle from Text");
+        loadText.setMinSize(150, 50);
+        loadText.prefHeightProperty().bind(mainPane.heightProperty());
+        loadText.setMaxSize(150, 150);
+
+        Button changeFont = new Button("Change Font");
+        changeFont.setMinSize(150, 50);
+        changeFont.prefHeightProperty().bind(mainPane.heightProperty());
+        changeFont.setMaxSize(150, 150);
+
+        sideComponents.add(loadFile, 0, 0, 1, 1);
+        sideComponents.add(loadText, 0, 1, 1, 1);
+        sideComponents.add(changeFont, 0, 2, 1 ,1 );
+
+        //Add randomly generate button
+        HBox randHbox = new HBox();
+        Button randGen = new Button("Randomly Generate Puzzle");
+        randGen.setMinSize(130, 80);
+        randHbox.getChildren().add(randGen);
+        randHbox.setAlignment(Pos.CENTER);
         //endregion
 
         //Graphics Context & Drawing
@@ -82,9 +133,13 @@ public class Main extends Application {
         //Adding to the Main Pane
         mainPane.add(canvasPane, 0, 0, 1, 1);
         mainPane.add(bottomComponents, 0, 1, 1, 1);
+        mainPane.add(sideComponents, 1, 0, 1, 1);
+        mainPane.add(randHbox, 1, 1, 1, 1);
 
         //Set the scene and show it
-        Scene scene = new Scene(mainPane, 600, 600);
+        Scene scene = new Scene(mainPane, 670, 600);
+        primaryStage.setMinWidth(700);
+        primaryStage.setMinHeight(650);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
