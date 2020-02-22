88d149d8caeb467d6539f8825ac9c3b383dc5430
tsvetanov i. (it4u19) <it4u19@soton.ac.uk>
2020-02-22 18:33:54 +0000

    More on coursework

--- Programmin II/Coursework/src/sample/Main.java
+++ Programmin II/Coursework/src/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -16,16 +18,29 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Main extends Application {
 
+    private ArrayList<Button> buttons = new ArrayList<>();
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("MathDoku");
 
         GridPane mainPane = new GridPane();
+        mainPane.setGridLinesVisible(true);
 
         //region Setting up the Canvas
         //Create the canvas and add it to its pane
@@ -33,7 +48,6 @@ public class Main extends Application {
         canvasPane.prefWidthProperty().bind(mainPane.widthProperty());
         canvasPane.prefHeightProperty().bind(mainPane.heightProperty());
         canvasPane.setAlignment(Pos.CENTER);
-
 
         //Set the border of the canvas
         canvasPane.setStyle("-fx-padding: 10;" +
@@ -100,13 +114,13 @@ public class Main extends Application {
         sideComponents.setVgap(20);
         sideComponents.setHgap(20);
 
-        int minHeight = (int)(sideComponents.heightProperty().getValue()/50);
-        Button loadFile = new Button ("Load Puzzle from File");
+        int minHeight = (int) (sideComponents.heightProperty().getValue() / 50);
+        Button loadFile = new Button("Load Puzzle from File");
         loadFile.setMinSize(150, 50);
         loadFile.prefHeightProperty().bind(mainPane.heightProperty());
         loadFile.setMaxSize(150, 150);
 
-        Button loadText = new Button ("Load Puzzle from Text");
+        Button loadText = new Button("Load Puzzle from Text");
         loadText.setMinSize(150, 50);
         loadText.prefHeightProperty().bind(mainPane.heightProperty());
         loadText.setMaxSize(150, 150);
@@ -116,9 +130,20 @@ public class Main extends Application {
         changeFont.prefHeightProperty().bind(mainPane.heightProperty());
         changeFont.setMaxSize(150, 150);
 
+        //TESTING
+        Button buttonTest = new Button("1");
+        buttonTest.setStyle(
+                "-fx-background-radius: 5em; " +
+                        "-fx-min-width: 10px; " +
+                        "-fx-min-height: 10px; " +
+                        "-fx-max-width: 10px; " +
+                        "-fx-max-height: 10px;"
+        );
+
         sideComponents.add(loadFile, 0, 0, 1, 1);
         sideComponents.add(loadText, 0, 1, 1, 1);
-        sideComponents.add(changeFont, 0, 2, 1 ,1 );
+        sideComponents.add(changeFont, 0, 2, 1, 1);
+        sideComponents.add(buttonTest, 0, 3, 1, 1);
 
         //Add randomly generate button
         HBox randHbox = new HBox();
@@ -129,17 +154,41 @@ public class Main extends Application {
         //endregion
 
         //region Draw Game Grid
-        int size = 3;
+        int size = 4;
         int width = 60;
-        for(int i = 0; i < size; i++) {
-            for(int j = 0; j < size; j++) {
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                //Add the rectangles
                 Rectangle rec = new Rectangle(width, width);
                 rec.setStroke(Color.BLACK);
                 rec.setFill(Color.WHITE);
-                rec.relocate(i * width, j * width);
-                canvasPane.add(rec, i, j, 1, 1);
+
+                //Add the text areas
+                TextField text = new TextField("");
+                text.setAlignment(Pos.CENTER);
+                text.maxWidthProperty().bind(canvasPane.widthProperty().divide(size + 1));
+                text.maxHeightProperty().bind(canvasPane.heightProperty().divide(size + 1));
+
+                //Stacks the textfield on top of the rectangles
+                StackPane stackPane = new StackPane();
+                stackPane.getChildren().add(rec);
+                stackPane.getChildren().add(text);
+
+                canvasPane.add(stackPane, i, j, 1, 1);
+
+
+                text.setFont(Font.font("Verdana", FontWeight.BOLD, 30));
+
                 rec.widthProperty().bind(canvasPane.widthProperty().divide(size + 1));
                 rec.heightProperty().bind(canvasPane.heightProperty().divide(size + 1));
+
+                text.setOnMouseClicked(e -> {
+                    buttonTest.setOnMouseClicked(f -> {
+                        text.setText(buttonTest.getText());
+                    });
+                });
+
             }
         }
         //endregion
@@ -156,13 +205,160 @@ public class Main extends Application {
         primaryStage.setMinHeight(670);
         primaryStage.setScene(scene);
         primaryStage.show();
+        randGen.requestFocus();
     }
-
-    private void drawShapes(GraphicsContext gc) {
-    }
-
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
+//USEFUL
+/*
+Button openNumPad = new Button("Open Number Pad");
+        openNumPad.setMinSize(150, 50);
+        openNumPad.prefHeightProperty().bind(mainPane.heightProperty());
+        openNumPad.setMaxSize(150, 150);
+
+ //region Add numPad button functionality
+        openNumPad.setOnMouseClicked(e -> {
+            GridPane keypadPane = new GridPane();
+            keypadPane.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
+            keypadPane.setHgap(50);
+            keypadPane.setVgap(30);
+            keypadPane.setPadding(new Insets(50));
+            keypadPane.setAlignment(Pos.TOP_CENTER);
+
+            //Add buttons to the window
+
+            //region Creating Buttons
+            Button button1 = new Button("1");
+            button1.setStyle(
+                    "-fx-background-radius: 5em; " +
+                            "-fx-min-width: 60px; " +
+                            "-fx-min-height: 60px; " +
+                            "-fx-max-width: 60px; " +
+                            "-fx-max-height: 60px;"
+            );
+
+            Button button2 = new Button("2");
+            button2.setStyle(
+                    "-fx-background-radius: 5em; " +
+                            "-fx-min-width: 60px; " +
+                            "-fx-min-height: 60px; " +
+                            "-fx-max-width: 60px; " +
+                            "-fx-max-height: 60px;"
+            );
+
+            Button button3 = new Button("3");
+            button3.setStyle(
+                    "-fx-background-radius: 5em; " +
+                            "-fx-min-width: 60px; " +
+                            "-fx-min-height: 60px; " +
+                            "-fx-max-width: 60px; " +
+                            "-fx-max-height: 60px;"
+            );
+
+            Button button4 = new Button("4");
+            button4.setStyle(
+                    "-fx-background-radius: 5em; " +
+                            "-fx-min-width: 60px; " +
+                            "-fx-min-height: 60px; " +
+                            "-fx-max-width: 60px; " +
+                            "-fx-max-height: 60px;"
+            );
+
+            Button button5 = new Button("5");
+            button5.setStyle(
+                    "-fx-background-radius: 5em; " +
+                            "-fx-min-width: 60px; " +
+                            "-fx-min-height: 60px; " +
+                            "-fx-max-width: 60px; " +
+                            "-fx-max-height: 60px;"
+            );
+
+            Button button6 = new Button("6");
+            button6.setStyle(
+                    "-fx-background-radius: 5em; " +
+                            "-fx-min-width: 60px; " +
+                            "-fx-min-height: 60px; " +
+                            "-fx-max-width: 60px; " +
+                            "-fx-max-height: 60px;"
+            );
+
+            Button button7 = new Button("7");
+            button7.setStyle(
+                    "-fx-background-radius: 5em; " +
+                            "-fx-min-width: 60px; " +
+                            "-fx-min-height: 60px; " +
+                            "-fx-max-width: 60px; " +
+                            "-fx-max-height: 60px;"
+            );
+
+            Button button8 = new Button("8");
+            button8.setStyle(
+                    "-fx-background-radius: 5em; " +
+                            "-fx-min-width: 60px; " +
+                            "-fx-min-height: 60px; " +
+                            "-fx-max-width: 60px; " +
+                            "-fx-max-height: 60px;"
+            );
+            //endregion
+
+            keypadPane.add(button1, 0, 0, 1, 1);
+            keypadPane.add(button2, 1, 0, 1, 1);
+            keypadPane.add(button3, 0, 1, 1, 1);
+            keypadPane.add(button4, 1, 1, 1, 1);
+            keypadPane.add(button5, 0, 2, 1, 1);
+            keypadPane.add(button6, 1, 2, 1, 1);
+            keypadPane.add(button7, 0, 3, 1, 1);
+            keypadPane.add(button8, 1, 3, 1, 1);
+
+            buttons.add(button1);
+            buttons.add(button2);
+            buttons.add(button3);
+            buttons.add(button4);
+            buttons.add(button5);
+            buttons.add(button6);
+            buttons.add(button7);
+            buttons.add(button8);
+
+            // New window (Stage)
+            Scene keypadScene = new Scene(keypadPane, 250, 430);
+            Stage newWindow = new Stage();
+            newWindow.initModality(Modality.NONE);
+            newWindow.setTitle("Keypad");
+            newWindow.setScene(keypadScene);
+
+            // Set position of second window, related to primary window.
+            newWindow.setX(primaryStage.getX() + primaryStage.getWidth());
+            newWindow.setY(primaryStage.getY());
+
+            newWindow.show();
+        });
+        //endregion
+
+ */
+
+
+/*
+for(Button button : buttons) {
+                System.out.println("setting text " + button.getText());
+                button.setOnMouseClicked(e ->{
+                    if(text.isFocused())
+                        text.setText(button.getText());
+
+                });
+            }
+ */
+
+
+                /*
+                //Change the color of the background
+                text.textProperty().addListener(new ChangeListener<String>() {
+                    @Override
+                    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                        rec.setFill(Color.RED);
+                        text.setStyle("-fx-background-color: red;");
+                    }
+                });*/
